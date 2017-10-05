eb87f971ef654b7509c9bf8cec5ced0f0b25fffa
Sergey Grigorev <xors.nn@gmail.com>
2017-10-05 15:33:34 +0300

    add simple gauge test

--- spec/datasource_spec.js
+++ spec/datasource_spec.js
@@ -4,6 +4,7 @@ import moment from 'moment';
 
 var testResp = require('./test_response.json');
 var testRespMultipleDS = require('./test_response_multiple_series.json');
+var testRespSimpleGauge = require('./test_response_simple_gauge.json');
 
 describe('GenericDatasource', function() {
   var ctx = {};
@@ -15,10 +16,6 @@ describe('GenericDatasource', function() {
     ctx.ds = new Datasource({}, ctx.$q, ctx.backendSrv, ctx.templateSrv);
   });
 
-  it('should pass a default dummy test', function(done) {
-    done();
-  });
-
   it('should return an empty array when no targets are set', function(done) {
     ctx.ds.query({targets: []}).then(function(result) {
       expect(result.data).to.have.length(0);
@@ -26,6 +23,7 @@ describe('GenericDatasource', function() {
     });
   });
 
+  // NOTE: timechart, single series
   it('should return the server results when a target is set', function(done) {
     ctx.backendSrv.datasourceRequest = function(request) {
       return ctx.$q.resolve({
@@ -65,6 +63,7 @@ describe('GenericDatasource', function() {
     });
   });
 
+  // NOTE: timechart, multiple series
   it('should return the server results when a target is set, and multiple series are returned', function(done) {
     ctx.backendSrv.datasourceRequest = function(request) {
       return ctx.$q.resolve({
@@ -104,7 +103,43 @@ describe('GenericDatasource', function() {
     });
   });
 
-  // TODO: 
-  // implement tests for:
-  // 2. simple gauge
+  // NOTE: simple gauge
+  it('should return the server results when a target is set for simple gauge visualisation', function(done) {
+    ctx.backendSrv.datasourceRequest = function(request) {
+      return ctx.$q.resolve({
+        _request: request,
+        data: testRespSimpleGauge
+      });
+    };
+
+    ctx.templateSrv.replace = function(data) {
+      return data;
+    }
+
+    var theQuery = ctx.ds.query({
+      targets: [
+        {
+          humioQuery:'count()', 
+          humioDataspace:'humio'
+        }
+      ],
+      panelId: 1,
+      range: {
+        'from': moment('2017-10-03T07:28:28.363Z'),
+        'to': moment('2017-10-03T13:28:28.363Z'),
+        'raw': {
+          'from': 'now-6h',
+          'to': 'now'
+        }
+      }
+    });
+
+    theQuery.then((result) => {
+      expect(result.data).to.have.length(1);
+      let series1 = result.data[0];
+      expect(series1.target).to.equal('_count');
+      expect(series1.datapoints.length).to.equal(1);
+      done();
+    });
+  });
 });
